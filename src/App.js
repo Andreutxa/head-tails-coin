38ec6685ab40a6a7f0bb6d3f2f944d9f2b33a428
Andrea Astorga <andrea.astorga95@gmail.com>
2021-03-25 21:33:40 +0100

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 export class App {
   minimumNumCoinsReversed (coinsArray) {
-    let coinsChanged = 0
+    let sumChangingPossibility1 = 0;
+    let sumChangingPossibility2 = 0;
 
-    for(let i = 0; i < coinsArray.length; i++) {
-      if(coinsArray[i] == coinsArray[i+1]) {
-        coinsChanged++
-      }
+    for (let i = 0; i < coinsArray.length; i++) {
+      (i % 2 === 0) ? (0 !== coinsArray[i]) ? sumChangingPossibility1++ : sumChangingPossibility2++ : (1 !== coinsArray[i]) ? sumChangingPossibility1++ : sumChangingPossibility2++
+      // if (i % 2 === 0) {
+      //   (0 !== coinsArray[i]) ? sumChangingPossibility1++ : sumChangingPossibility2++
+      // } else {
+      //   (1 !== coinsArray[i]) ? sumChangingPossibility1++ : sumChangingPossibility2++
+      // }
     }
 
-    return coinsChanged
+    return (sumChangingPossibility1 > sumChangingPossibility2) ? sumChangingPossibility2 : sumChangingPossibility1
+
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
 import { App } from './App';
 describe('App tests', () => {
   const app = new App();
-  it ('Test 1: Should print => 1', () => {
+  it ('Test 1: Should print => 0', () => {
     const response = app.minimumNumCoinsReversed([1,0,1]);
     expect(response).toBe(0)
+  })
+  it ('Test 2: Should print => 1', () => {
+    const response = app.minimumNumCoinsReversed([1,0,1,0,1,1]);
+    expect(response).toBe(1)
+  })
+  it ('Test 3: Should print => 2', () => {
+    const response = app.minimumNumCoinsReversed([1,1,0,1,1]);
+    expect(response).toBe(2)
+  })
+  it ('Test 4: Should print => 2', () => {
+    const response = app.minimumNumCoinsReversed([0,1,1,0]);
+    expect(response).toBe(2)
+  })
+  it ('Test 5: Should print => 2', () => {
+    const response = app.minimumNumCoinsReversed([1,1,1,1]);
+    expect(response).toBe(2)
+  })
+  it ('Test 6: Should print => 2', () => {
+    const response = app.minimumNumCoinsReversed([1,1,1,1,1,0]);
+    expect(response).toBe(2)
+  })
+  it ('Test 7: Should print => 4', () => {
+    const response = app.minimumNumCoinsReversed([0,1,1,1,1,1,1,1,1,1,0]);
+    expect(response).toBe(4)
   })
 })
