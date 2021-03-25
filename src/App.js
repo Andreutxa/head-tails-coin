5a1ada526c9ff91abe826663b70ebb3eeea75f7b
Andrea Astorga <andrea.astorga95@gmail.com>
2021-03-25 19:53:42 +0100

    first test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+export class App {
+  minimumNumCoinsReversed (coinsArray) {
+    let coinsChanged = 0
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    for(let i = 0; i < coinsArray.length; i++) {
+      if(coinsArray[i] == coinsArray[i+1]) {
+        coinsChanged++
+      }
+    }
+
+    return coinsChanged
+  }
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { App } from './App';
+describe('App tests', () => {
+  const app = new App();
+  it ('Test 1: Should print => 1', () => {
+    const response = app.minimumNumCoinsReversed([1,0,1]);
+    expect(response).toBe(0)
+  })
+})
